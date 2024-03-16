25e33c7917e7d203e1b066ecbe0ae492fcddf0c4
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-03-16 22:11:41 +0100

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Laboratorium 1
-        //1
-        Point point1=new Point(2,2);
-        Point point2=new Point(4,2);
-        Segment segment =new Segment(point2,point1);
-        System.out.println(segment.lengthSection()+ "\n");
-        //2
-        System.out.println(segment.toSvg()+ "\n");
-        //3
-        Point point = new Point(3,4);
-        Segment []arr=Segment.perpendicular(segment,point,3);
-        //4
-        Point[] arr_points=new Point[3];
-        for(int i = 0; i < arr_points.length; ++i){
-            arr_points[i]=new Point(1,3);
-        }
-        Polygon polygon =new Polygon(arr_points);
-        polygon.print();
+        // LB 2; Task 1
+        Point point1=new Point(1,3);
+        Point point2=new Point(3,1);
+
+        Style style=new Style("red","blue",2.0);
+        Polygon polygon =new Polygon(new Point[]{point1,point2},style);
+        System.out.println(polygon.toSvg());
+
+        Style style1=new Style(null,null,2.0);
+        Polygon polygon1 =new Polygon(new Point[]{point1,point2},style1);
+        System.out.println(polygon1.toSvg());
     }
 }
--- src/Polygon.java
+++ src/Polygon.java
@@ -3,12 +3,10 @@ import java.util.Locale;
 
 public class Polygon {
     private Point[] points;
-    public Polygon(Point[] otherPoints) {
-        ///this.points = points;
-        this.points=new Point[otherPoints.length];
-        for (int i=0;i<otherPoints.length;++i){
-            this.points[i]=new Point(otherPoints[i].x,otherPoints[i].y);//deep copy of the object
-        }
+    Style style;
+    public Polygon(Point[] points, Style style) {
+        this.points=points;
+        this.style=style;
     }
     public void print(){
         for(Point point : points) {
@@ -20,7 +18,6 @@ public class Polygon {
         for(Point point : points){
             pointsString += point.x +","+point.y+" ";
         }
-        return String.format(Locale.ENGLISH,"<line points=\"%s\" " +
-                "style=\"stoke:blue,stoke-width:2\" />",pointsString);
+        return String.format(Locale.ENGLISH,"<line points=\"%s\"%s />",pointsString,style.toSvg());
     }
 }
