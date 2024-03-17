7d345f38f1cd0ea69f228faa6cf4dc3474cd4a55
VasylIshchuk <Vasyl.Ishchuk@live.umcs.edu.pl>
2024-03-17 15:40:21 +0100

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // LB 2; Task 2
-        Point point1 = new Point(100,0);
-        Point point2 = new Point(50,100);
-        Point point3 = new Point(150,100);
-        Style style1=new Style("pink",null,2.0);
-        Polygon polygon1 =new Polygon(new Point[]{point1,point2,point3},style1);
+        // LB 2; Task 3
+        Point point1 = new Point(0,100);
+        Point point2 = new Point(100,102);
+        Segment segment =new Segment(point1,point2);
+        Style style=new Style("pink",null,2.0);
+        Polygon polygon = Polygon.square(segment,style);
         SvgScene svgScene = new SvgScene();
-        svgScene.addPolygon(polygon1);
+        svgScene.addPolygon(polygon);
         svgScene.saveHtml("./test.html");
     }
 }
--- src/Polygon.java
+++ src/Polygon.java
@@ -8,6 +8,17 @@ public class Polygon {
         this.points=points;
         this.style=style;
     }
+    public Polygon(int count, Style style) {
+        this.points=new Point[count];
+        this.style=style;
+    }
+    public void setPoints(Point[] points) {
+        this.points = points;
+    }
+    public void setPoint(int index,Point points) {
+        this.points[index] = points;
+    }
+
     public void print(){
         for(Point point : points) {
             System.out.println(point.x +" "+ point.y );
@@ -19,5 +30,18 @@ public class Polygon {
             pointsString += point.x +","+point.y+" ";
         }
         return String.format(Locale.ENGLISH,"<polygon points=\"%s\"%s />",pointsString,style.toSvg());
+    }
+    public static Polygon square(Segment line, Style style){
+        double x=(line.getPoint1().x+ line.getPoint2().x)/2;
+        double y=(line.getPoint1().y+ line.getPoint2().y)/2;
+        Point center = new Point(x,y);
+        Segment [] segments = Segment.perpendicular(line,center,line.lengthSection()/2);
+
+        Polygon square = new Polygon(4,style);
+        square.setPoint(0,line.getPoint1());
+        square.setPoint(1,segments[0].getPoint2());
+        square.setPoint(2,line.getPoint2());
+        square.setPoint(3,segments[1].getPoint2());
+        return square;
     }
 }
